3140975729970f604f8c6ccee021e0426d547c10
Ben Halverson <benhalverson33@gmail.com>
2025-03-19 02:51:57 -0700

    wip

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from websockets.exceptions import ConnectionClosed
+import uvicorn
+
+
 import asyncio
 import uvicorn
 
@@ -15,25 +19,45 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     print('WebSocket connected')
 
+
     try:
+        # send "Connection established" message to client
+        await websocket.send_text("Connection established!")
+        
+        # await for messages and send messages
         while True:
-            data = await websocket.receive_json()
-            print('Data received:', data)
+            msg = await websocket.receive_text()
+            if msg.lower() == "close":
+                await websocket.close()
+                break
+            else:
+                print(f'CLIENT says - {msg}')
+                await websocket.send_text(f"Your message was: {msg}")
+                
+    except (WebSocketDisconnect, ConnectionClosed):
+        print("Client disconnected")
+    # try:
+    #     while True:
+    #         data = await websocket.receive_json()
+    #         print('Data received:', data)
 
-            right_trigger = data.get('right_trigger', 0.0)
-            left_axis = data.get('left_axis', 0.0)
+    #         right_trigger = data.get('right_trigger', 0.0)
+    #         left_axis = data.get('left_axis', 0.0)
 
-            print(f'Right Trigger: {right_trigger}, Left Axis: {left_axis}')
+    #         print(f'Right Trigger: {right_trigger}, Left Axis: {left_axis}')
 
-            # Send acknowledgment back to the client
-            await websocket.send_json({"message": "Data received", "right_trigger": right_trigger, "left_axis": left_axis})
+    #         await websocket.receive_text({"message": "Data received", "right_trigger": right_trigger, "left_axis": left_axis})
 
-    except WebSocketDisconnect as e:
-        print(f'WebSocket disconnected: {e.code}')
-    except Exception as e:
-        print(f'Unexpected WebSocket error: {e}')
-    finally:
-        print('Cleaning up WebSocket connection...')
+    # except WebSocketDisconnect as e:
+    #     print(f'WebSocket disconnected: {e.code}')
+    #     await asyncio.sleep(0.5)
+    # except Exception as e:
+    #     print(f'WebSocket disconnected: {e.code} - Reason: {getattr(e, "reason", "Unknown")}')
+    #     await asyncio.sleep(0.5)
+
+    # finally:
+    #     print('Cleaning up WebSocket connection...')
+    #     await asyncio.sleep(0.5)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0" , port=8000)
+    uvicorn.run(app, host="0.0.0.0" , port=8000, log_level="debug")
